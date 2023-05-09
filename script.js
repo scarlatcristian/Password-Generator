646d97dc04effded3503e1fef68cfe9fee0d008d
cristi <scarlat.cristian.gabriel@gmail.com>
2023-05-09 19:20:13 +0300

    created get random functions

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const getRandomLower = () => {
+  // returns a string created from the specified sequences of UTF-16
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getRandomUpper = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getRandomNumber = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+const getRandomSymbol = () => {
+  const symbols = `!@#$%^&*(){}[]=<>/,.`;
+  return symbols[Math.floor(MAth.random() * symbols.length)];
+};
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
