ba5a088b66bce8e734dfba17633f802c882f5a52
cristi <scarlat.cristian.gabriel@gmail.com>
2023-05-09 20:08:11 +0300

    copy password to clipboard

--- script.js
+++ script.js
@@ -82,3 +82,17 @@ generateEl.addEventListener("click", () => {
     length
   );
 });
+
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+
+  if (!password) return;
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard");
+});
