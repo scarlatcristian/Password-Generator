3c534995b56cb17092560a6fcff75c996f17cd41
cristi <scarlat.cristian.gabriel@gmail.com>
2023-05-09 20:02:56 +0300

    added generatePassword and randomizeString

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
 const getRandomLower = () => {
   // returns a string created from the specified sequences of UTF-16
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -15,7 +24,7 @@ const getRandomNumber = () => {
 
 const getRandomSymbol = () => {
   const symbols = `!@#$%^&*(){}[]=<>/,.`;
-  return symbols[Math.floor(MAth.random() * symbols.length)];
+  return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
 const randomFunc = {
@@ -24,3 +33,52 @@ const randomFunc = {
   number: getRandomNumber,
   symbol: getRandomSymbol,
 };
+
+const randomizeString = (str) => {
+  const chars = str.split("");
+
+  for (let i = 0; i < chars.length; i++) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [chars[i], chars[j]] = [chars[j], chars[i]];
+  }
+
+  return chars.join("");
+};
+
+const generatePassword = (lower, upper, number, symbol, length) => {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  //   Filter anything that has false as a value
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) return "";
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+  return randomizeString(finalPassword);
+};
+
+// Event handlers
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
